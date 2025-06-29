ba972d88889c503faeca86e77eca2a1294e40823
legerol TCHOGBE <legerolt@gmail.com>
2025-06-29 23:22:36 +0100

    Ajout des onglets storie et des réaction sur les pubs la page d'accueil

--- vues/clients/accueil.php
+++ vues/clients/accueil.php
@@ -8,9 +8,6 @@
     <title>Fil d'actualité Facebook</title>
     <link rel="stylesheet" href="/php-social-network/assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <style>
-
-    </style>
 </head>
 
 <body>
